e0a8854d5781d9c920145620fa19c6c753274f78
Viktoria-W <70846354+Viktoria-W@users.noreply.github.com>
2020-10-20 22:26:16 +0300

    Добавляем интерактивную карту

--- js/script.js
+++ js/script.js
@@ -5,50 +5,50 @@ const checkInDate = modalWindow.querySelector(".check-in-date");
 const searchForm = modalWindow.querySelector(".form-window");
 const checkOutDate = modalWindow.querySelector(".check-out-date");
 const adults = modalWindow.querySelector(".number-adults");
-const children =  modalWindow.querySelector(".number-children");
+const children = modalWindow.querySelector(".number-children");
 const findButton = modalWindow.querySelector(".find");
 
 let isStorageSupport = true;
 const storage = "";
 
 try {
-  storage = localStorage.getItem("login");
+	storage = localStorage.getItem("login");
 } catch (err) {
-  isStorageSupport = false;
+	isStorageSupport = false;
 }
- 
-searchButton.addEventListener("click", function (evt) { 
-if (modalWindow.classList.contains("modal-show")) {
-    evt.preventDefault();
-    modalWindow.classList.remove("modal-show");
-    modalWindow.classList.add("modal-animation");
-} else {
-    evt.preventDefault();
-    modalWindow.classList.add("modal-show"); 
-    modalWindow.classList.remove("modal-animation");
-    }
-if (storage) {
-    adults.value = storage;
-    children.value = storage;
-}
+
+searchButton.addEventListener("click", function (evt) {
+	if (modalWindow.classList.contains("modal-show")) {
+		evt.preventDefault();
+		modalWindow.classList.remove("modal-show");
+		modalWindow.classList.add("modal-animation");
+	} else {
+		evt.preventDefault();
+		modalWindow.classList.add("modal-show");
+		modalWindow.classList.remove("modal-animation");
+	}
+	if (storage) {
+		adults.value = storage;
+		children.value = storage;
+	}
 });
 
 searchForm.addEventListener("submit", function (evt) {
-    if (!checkInDate.value || !checkOutDate.value || !adults.value){
-        evt.preventDefault();
-    } else {
-        if (isStorageSupport) {
-        evt.preventDefault();
-        localStorage.setItem("adults", adults.value, "children", children.value );
-        }
-    }
+	if (!checkInDate.value || !checkOutDate.value || !adults.value) {
+		evt.preventDefault();
+	} else {
+		if (isStorageSupport) {
+			evt.preventDefault();
+			localStorage.setItem("adults", adults.value, "children", children.value);
+		}
+	}
 });
-   
-window.addEventListener("keydown", function (evt){
-    if (evt.keyCode === 27) {
-        if (modalWindow.classList.contains("modal-show")) {
-        evt.preventDefault();
-        modalWindow.classList.remove("modal-show");
-        }
-    }
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		if (modalWindow.classList.contains("modal-show")) {
+			evt.preventDefault();
+			modalWindow.classList.remove("modal-show");
+		}
+	}
 });

f5362a645fdcad41c8e6dbbcbec3eccb1a1aeb4c
Viktoria-W <70846354+Viktoria-W@users.noreply.github.com>
2020-10-26 23:25:53 +0300

    Изменения после первой защиты #3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const searchButton = document.querySelector(".search-button");
-const closeButton = document.querySelector(".booking-window");
 const modalWindow = document.querySelector(".booking-window");
 const checkInDate = modalWindow.querySelector(".check-in-date");
 const searchForm = modalWindow.querySelector(".form-window");
@@ -7,6 +6,8 @@ const checkOutDate = modalWindow.querySelector(".check-out-date");
 const adults = modalWindow.querySelector(".number-adults");
 const children = modalWindow.querySelector(".number-children");
 const findButton = modalWindow.querySelector(".find");
+
+modalWindow.classList.remove("modal-show");
 
 let isStorageSupport = true;
 const storage = "";
@@ -34,10 +35,13 @@ searchButton.addEventListener("click", function (evt) {
 searchForm.addEventListener("submit", function (evt) {
 	if (!checkInDate.value || !checkOutDate.value || !adults.value) {
 		evt.preventDefault();
+		modalWindow.classList.remove("modal-error");
+		modalWindow.offsetWidth = modalWindow.offsetWidth;
+		modalWindow.classList.add("modal-error");
 	} else {
 		if (isStorageSupport) {
 			evt.preventDefault();
-			localStorage.setItem("adults", adults.value, "children", children.value);
+			localStorage.setItem("adults", adults.value, "children", children.value);	
 		}
 	}
 });
@@ -47,6 +51,7 @@ window.addEventListener("keydown", function (evt) {
 		if (modalWindow.classList.contains("modal-show")) {
 			evt.preventDefault();
 			modalWindow.classList.remove("modal-show");
+			modalWindow.classList.remove("modal-error");
 		}
 	}
 });
